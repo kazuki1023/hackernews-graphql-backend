58b95ab0d264cbc2aa82a1a5a670a1603e0e0e38
kazuki1023 <k.keio12569@gmail.com>
2023-11-29 00:49:44 +0900

    apolloServerをインスタンス化した

--- src/server.ts
+++ src/server.ts
@@ -15,3 +15,10 @@ const resolvers = {
     info: () => `HackerNewsクローン`
   },
 };
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server.listen().then(({url}: { url: string }) => console.log(`Server is running on ${url}`));
