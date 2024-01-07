1663143caa285c312856541325af016ef711651e
kazuki1023 <k.keio12569@gmail.com>
2024-01-07 22:30:37 +0900

    tokenからidを取得するための関数作成

---
+++ src/utils.ts
@@ -0,0 +1,33 @@
+// tokenを複合するための関数
+function getTokenPayload(token: any) {
+  // token化されたもののの前の情報を復元する
+  return jwt.verify(token, APP_SECRET);
+}
+
+
+// ユーザーid取得のための関数
+function getUserId(req: any, authToken?: any) {
+    if (req) {
+      // (認証権限があるかどうか記載がある)ヘッダーを確認する
+        const authHeader = req.headers.authorization;
+        if (authHeader) {
+          // 不要なbearerを削除して、トークンを取得する
+            const token = authHeader.replace('Bearer ', '');
+            if (!token) {
+                throw new Error('No token found');
+            }
+            // そのtokanを複合する
+            const { userId } = getTokenPayload(token);
+            return userId;
+        }
+    } else if (authToken) {
+        const { userId } = getTokenPayload(authToken);
+        return userId;
+    }
+    throw new Error('Not authenticated');
+}
+
+module.exports = {
+    APP_SECRET,
+    getUserId,
+};
